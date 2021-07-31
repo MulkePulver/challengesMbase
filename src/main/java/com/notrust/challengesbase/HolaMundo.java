8ef819adf53ff1dfdb191cc85abe692bb8965bd6
mulkepulv <juanandres.garcia.duarte@gmail.com>
2021-07-31 12:43:35 -0500

    Test3

--- src/main/java/com/notrust/challengesbase/HolaMundo.java
+++ src/main/java/com/notrust/challengesbase/HolaMundo.java
@@ -14,9 +14,9 @@ import java.util.Scanner;
 public class HolaMundo {
     public static void main (String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Ingrese un número");
-        int n = sc.nextInt();
-        int m = n * 2;
+        System.out.println("Ingrese un número: ");
+        double n = sc.nextDouble();
+        double m = n * 2;
         System.out.println("Resultado: " + m);
     }
 }
