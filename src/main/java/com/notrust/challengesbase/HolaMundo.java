1b7f9ec99174601e53e08183c998290675782317
mulkepulv <juanandres.garcia.duarte@gmail.com>
2021-07-31 13:49:36 -0500

    Test4

--- src/main/java/com/notrust/challengesbase/HolaMundo.java
+++ src/main/java/com/notrust/challengesbase/HolaMundo.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.notrust.challengesbase;
 
 import java.util.Scanner;
 
-/**
- *
- * @author Andrés
- */
 public class HolaMundo {
     public static void main (String[] args){
         Scanner sc = new Scanner(System.in);
