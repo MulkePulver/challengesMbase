b1b565c84f67d35c69d763aee81ee32d483ea722
mulkepulv <juanandres.garcia.duarte@gmail.com>
2021-07-31 12:35:05 -0500

    Test

---
+++ src/main/java/com/notrust/challengesbase/HolaMundo.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.notrust.challengesbase;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Andrés
+ */
+public class HolaMundo {
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese un número");
+        int x = 0; 
+    }
+}
