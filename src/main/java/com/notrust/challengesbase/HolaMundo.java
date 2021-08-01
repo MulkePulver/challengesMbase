b1e3f48f3f80eee4a686dc552d735ca6b058dfc5
mulkepulv <juanandres.garcia.duarte@gmail.com>
2021-07-31 22:07:15 -0500

    Aún no se sabe cuál va a ser la persona indicada para la impresión

--- src/main/java/com/notrust/challengesbase/HolaMundo.java
+++ src/main/java/com/notrust/challengesbase/HolaMundo.java
@@ -2,12 +2,22 @@ package com.notrust.challengesbase;
 
 import java.util.Scanner;
 
-public class HolaMundo {
+public class HolaMundo { 
     public static void main (String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Ingrese un número: ");
-        double n = sc.nextDouble();
-        double m = n * 2;
-        System.out.println("Resultado: " + m);
+        
+        System.out.println("Ingrese la edad de Paco: ");
+        int EdadHugo = 30;
+        int EdadPaco = sc.nextInt(EdadHugo / 2) -4;
+        int EdadLuis = (EdadHugo + EdadPaco) * 5;
+        System.out.println("Edad de Paco, Hugo y Luis:" + EdadPaco + " " + EdadHugo + " " + EdadLuis);
+        if (EdadLuis <= 20)
+            System.out.println("Se encuentra en etapa 1");
+        else if (EdadLuis <= 30)
+            System.out.println("Se encuentra en etapa 2");
+        else if (EdadLuis <= 50)
+            System.out.println("Se encuentra en etapa 3");
+        else if (EdadLuis >= 50)
+            System.out.println("Se encuentra en etapa 4");
     }
 }
